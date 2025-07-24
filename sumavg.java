31b1b9516d7b59c1ffc3b33d87c38500d800358e
rahul gillella <rahulgillella05@gmail.com>
2025-07-24 19:51:11 +0530

    added the java code

---
+++ sumavg.java
@@ -0,0 +1,12 @@
+public class SumAverage {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        double average = sum / 2.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
