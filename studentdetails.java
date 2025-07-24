19f69c9d7b5cb4d6db80e89dbd0faa81945536b4
rahul gillella <rahulgillella05@gmail.com>
2025-07-24 21:30:03 +0530

    error solve

--- studentdetails.java
+++ studentdetails.java
@@ -1,16 +1,13 @@
-public class studentdetails{
+public class studentdetails {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: java StudentDetails <name> <rollno> <dept>");
-            return;
-        }
-
-        String name = args[0];
-        String rollNo = args[1];
-        String dept = args[2];
+        String name = "Rahul";
+        int rollNo = 101;
+        String dept = "CSE";
 
         System.out.println("===== Student Details =====");
-        System.out.println("Name   : " + name);
+        System.out.println("Name: " + name);
         System.out.println("Roll No: " + rollNo);
-        System.out.println("Dept   : " + dept);
+        System.out.println("Department: " + dept);
         System.out.println("===========================");
+    }
+}
